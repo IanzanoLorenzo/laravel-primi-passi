c185beb9c75e8085ccf4b2c927fd0c55a34ad195
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-07-20 19:55:39 +0200

    aggiunta home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    
+    $data = [
+        'hello' => 'Ciao a te',
+        'from' => 'Me'
+    ];
+    
+    return view('home', $data);
 });
